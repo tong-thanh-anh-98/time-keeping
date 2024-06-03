f6aa60a7cf121d57d779d03db45f42803da7254a
DuongVanTuan <duong.van.tuan@uruca.jp>
2024-06-03 17:29:45 +0700

    feature: login user

---
+++ manage-app/app/Http/Controllers/User/AuthController.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Http\Controllers\User;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\Auth\LoginRequest;
+use Illuminate\Support\Facades\Auth;
+use App\Helpers\ConstCommon;
+
+class AuthController extends Controller
+{
+    //
+    public function showLoginForm(){
+        if(Auth::check()){
+            return redirect()->route('home')->with('Bạn đã đăng nhập rồi');
+        }
+        return view('user.login');
+    }
+
+    public function login(LoginRequest $request)
+    {
+        die();
+        $intendedUrl = session('url.intended');
+        $credentials = [
+            'name' => $request->input('name'),
+            'password' => $request->input('password'),
+        ];
+        
+        $redirectTo = route('home');
+        $message = "Đăng nhập thành công";
+        if (!Auth::attempt($credentials)) {
+            return redirect()->back()->with('error', "Sai thông tin đăng nhập, vui lòng nhập lại");
+        }
+        $request->session()->regenerate();
+
+        if (Auth::user()->type == ConstCommon::TypeUser && $intendedUrl && $intendedUrl != route('login')) {
+            $redirectTo = $intendedUrl;
+        }
+
+        return redirect()->to($redirectTo)->with('message', $message);
+    }
+
+    public function logOut(){
+        Auth::logout();
+        return redirect()->route('login');
+    }
+
+
+
+}
+
---
+++ manage-app/app/Http/Requests/Auth/LoginRequest.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Http\Requests\Auth;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class LoginRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules()
+    {
+        return [
+            'name' => 'required',
+            'email' => 'required',
+            'password' => [
+                'required',
+                'min:8',
+                'regex:/^(?=.*[!@#$%^&*()\-_=+{};:,<.>ยง~`|[\]\\/"\'])/'
+            ],
+        ];
+    }
+    public function messages()
+    {
+        return [
+            'name.required' => 'Vui lòng nhập',
+            'password.required' => 'Mật khẩu không được để trống',
+            'password.min' => 'Mật khẩu phải có độ dài tối thiểu là 8 ký tự',
+            'password.regex' => 'Mật khẩu phải chứa ít nhất một ký tự đặc biệt'
+        ];
+    }
+}
